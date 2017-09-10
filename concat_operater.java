3398027639b8e61e440a3cecf424e1389b693eb6
tanzeen <raziataj14@gmail.com>
2017-09-10 13:47:32 -0400

    comment insert

---
+++ concat_operater.java
@@ -0,0 +1,32 @@
+package sits;
+// putting comment
+public class concat_operater {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String x = "Hello";
+		String y = "to java";
+		//add the two strings x and y
+		System.out.println(x+y);
+		System.out.println(x+ " "+y);
+
+
+		int a = 10;
+		int b=49;
+		System.out.println(a+b);
+		System.out.println(a+b+" "+x+ " "+y);
+		System.out.println(a+b+ " "+x+ " "+y);
+		System.out.println(a+b+ " "+x+ " "+y);
+		System.out.println(a+b+ " "+x+ " "+y);
+
+		
+
+		
+		
+		
+		
+
+	}
+
+}
